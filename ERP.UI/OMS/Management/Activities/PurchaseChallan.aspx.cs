34659e09a20646b455987dc8349f34535a6fc871
PritiINT <62432969+PritiINT@users.noreply.github.com>
2023-03-13 13:07:57 +0530

    check in

--- ERP.UI/OMS/Management/Activities/PurchaseChallan.aspx.cs
+++ ERP.UI/OMS/Management/Activities/PurchaseChallan.aspx.cs
@@ -1,5 +1,7 @@
 ﻿#region//====================================================Revision History=========================================================================
-// 1.0   v4.0.37	Priti	04-03-2023	0025690:Alt Qty is not updating as per the main qty while entering Purchase GRN
+// 1.0   v2.0.37	Priti	04-03-2023	0025690:Alt Qty is not updating as per the main qty while entering Purchase GRN
+// 2.0   v2.0.37	Priti	13-03-2023	0025723:There is multiple rows inserted in warehouse tables while making stock batch wise in same warehouse in Purchase GRN
+
 #endregion//====================================================End Revision History=====================================================================
 
 
@@ -1075,27 +1077,27 @@ namespace ERP.OMS.Management.Activities
                     {
                         dt_PLQuote.ClientEnabled = false;
                     }
-
-                    string strNewVal = "", strOldVal = "", strProductType = "";
                     tempdt = dt.Copy();
-                    foreach (DataRow drr in tempdt.Rows)
-                    {
-                        strNewVal = Convert.ToString(drr["QuoteWarehouse_Id"]);
-                        strProductType = Convert.ToString(drr["ProductType"]);
 
-                        if (strNewVal == strOldVal)
-                        {
-                            drr["WarehouseName"] = "";
-                            drr["TotalQuantity"] = "0";
-                            //drr["BatchNo"] = "";
-                            drr["ViewBatch"] = "";
-                            drr["SalesQuantity"] = "";
-                            drr["ViewMfgDate"] = "";
-                            drr["ViewExpiryDate"] = "";
-                        }
-
-                        strOldVal = strNewVal;
-                    }
+                    //Rev 2.0
+                    //string strNewVal = "", strOldVal = "", strProductType = "";
+                    //foreach (DataRow drr in tempdt.Rows)
+                    //{
+                    //    strNewVal = Convert.ToString(drr["QuoteWarehouse_Id"]);
+                    //    strProductType = Convert.ToString(drr["ProductType"]);
+                    //    if (strNewVal == strOldVal)
+                    //    {
+                    //        drr["WarehouseName"] = "";
+                    //        drr["TotalQuantity"] = "0";
+                    //        //drr["BatchNo"] = "";
+                    //        drr["ViewBatch"] = "";
+                    //        drr["SalesQuantity"] = "";
+                    //        drr["ViewMfgDate"] = "";
+                    //        drr["ViewExpiryDate"] = "";
+                    //    }
+                    //    strOldVal = strNewVal;
+                    //}
+                    //Rev 2.0 End
 
                     tempdt.Columns.Remove("QuoteWarehouse_Id");
                     tempdt.Columns.Remove("ProductType");
